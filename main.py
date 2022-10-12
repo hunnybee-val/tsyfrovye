691c5543572dfa8b6452de77417326c7c1db1bd3
Lerra <valeria.strygina.home@gmail.com>
2022-10-12 14:45:01 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+lst = [1,2,3]
+lst.append(4)
+lst.remove(3)
+print(lst)
+print(lst[2])
+lst.remove(2)
+if 2 in lst:
+    print('2 is in da list')
+else:
+    print('2 is NOT in da list')
+
+tup = (1, 2, 3)
+
+print(tup)
+
+first, second, third = tup
+print("1", first)
+
+eng_rus_dict = {
+    "cat":"кот",
+    "car":"машина"
+}
+print(eng_rus_dict['cat'])
